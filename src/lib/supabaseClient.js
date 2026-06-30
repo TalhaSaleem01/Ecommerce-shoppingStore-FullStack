c17050ba49df15e4b5eef73c8aeb88dc3af5a777
Talha-Saleem-786 <tch31804@gmail.com>
2026-06-30 22:56:23 +0500

    fixing

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -9,5 +9,6 @@ if (!supabaseUrl || !supabaseAnonKey) {
     'Add them to your .env file at the project root, then restart `npm run dev`.'
   )
 }
-
+console.log("URL:", import.meta.env.VITE_SUPABASE_URL);
+console.log("KEY EXISTS:", !!import.meta.env.VITE_SUPABASE_ANON_KEY);
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
